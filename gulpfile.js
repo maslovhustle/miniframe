34f7da5eb4299c485d67dc3f750185b0ed6a45e8
maslovhustle <rarus36@gmail.com>
2016-05-26 03:27:59 +0300

    miniframe v 1.0

---
+++ gulpfile.js
@@ -0,0 +1,52 @@
+var gulp         = require('gulp');
+var sass         = require('gulp-sass');
+var concat       = require('gulp-concat');
+var sourcemaps   = require('gulp-sourcemaps');
+var bower        = require('gulp-bower');
+var del          = require('del');
+var autoprefixer = require('gulp-autoprefixer');
+var runSequence  = require('run-sequence');
+var jade         = require('gulp-jade');
+var addsrc       = require('gulp-add-src');
+var minify       = require('gulp-minify');
+var cleanCSS     = require('gulp-clean-css');
+
+
+
+gulp.task('sass', function(){
+    gulp.src('source/**/*.sass')
+        .pipe(sass())
+        .pipe(gulp.dest('public'))
+});
+
+gulp.task('styles', function(){
+    return gulp.src('public/**/*.css')
+        .pipe(sourcemaps.init())
+        .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9'))
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(concat('libstyle.min.css'))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('public'));
+});
+
+gulp.task('jade', function () {
+    gulp.src('source/**/*.jade')
+        .pipe(jade())
+        .pipe(gulp.dest('public'));
+});
+
+gulp.task('watch', function() {
+    gulp.watch('source/**/*.*', ['sass']);
+    gulp.watch('source/**/*.*', ['jade']);
+});
+
+
+gulp.task('clean', function(){
+        return del('public');
+});
+
+gulp.task('build', function() {
+    runSequence([ 'clean' ], 'sass', 'jade', 'styles', 'watch');
+});
+
+gulp.task('default', [ 'build' ]);
